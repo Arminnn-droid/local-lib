190982f315d3fbf0aaca6922073689dda3f1cf8c
Arminnn-droid <arminmalekm44@gmail.com>
2022-08-17 19:03:04 +0430

    admin update Book list display created also genre_display added to Book model

--- library/admin.py
+++ library/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
 from .models import Book, Author, BookInstance, Genre
 
-# Register your models here.
+#######################
 
-admin.site.register(Book)
-admin.site.register(BookInstance)
-admin.site.register(Author)
-admin.site.register(Genre)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+# admin.site.register(Book)
+# admin.site.register(BookInstance)
+# admin.site.register(Author)
+# admin.site.register(Genre)
