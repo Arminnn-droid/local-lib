bb3684a89518a2fca57a27462f61c0bee8d0af21
Arminnn-droid <arminmalekm44@gmail.com>
2022-08-17 20:06:55 +0430

    admin update -list display for Book BookInstance and author added -added BookInstanceInline

--- library/admin.py
+++ library/admin.py
@@ -3,17 +3,32 @@ from .models import Book, Author, BookInstance, Genre
 
 #######################
 
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 1
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
+    list_filter = ('author', 'genre')
+    inlines = [BooksInstanceInline]
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        ('information', {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
